ed63a5e9e4bcbdcd3c13f25b03f9965c75b1eadf
anderssonvivi <viviandersson0@gmail.com>
2024-04-01 12:11:46 +0200

    Total LOC

--- scripts/source_stats.sh
+++ scripts/source_stats.sh
@@ -12,9 +12,11 @@ for ((i = 0; i < ${#LANGS_POSTFIX[@]}; i++)); do
     LANG="${LANGS[i]}"
     POSTFIX="${LANGS_POSTFIX[i]}"
     cd "$ETH" || exit 1
-    git ls-files | grep "$POSTFIX" | xargs wc -l | tail -n 1 | awk -v lang="$LANG" '{print lang ": " $1}' >> "$OUT"
+    git ls-files | grep "$POSTFIX" | xargs wc -l | tail -n 1 | awk -v lang="$LANG" '{print lang ": " $1 " (LOC)"}' >> "$OUT"
 done
 
+TOTAL=$(awk '{sum += $2} END {print "Total:", sum}' "$OUT")
+echo "$TOTAL" >> "$OUT"
 # todo: file count with wc -l and awk '{print $1 - 1}', exept if 0 then 1
 # todo: total loc
 # todo: andel
