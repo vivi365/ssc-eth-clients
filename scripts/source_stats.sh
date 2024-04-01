69b6d6f5bd97a7b744651300b23766ee9f912f0d
anderssonvivi <viviandersson0@gmail.com>
2024-04-01 12:04:19 +0200

    script regex adjustment

--- scripts/source_stats.sh
+++ scripts/source_stats.sh
@@ -6,17 +6,18 @@ OUT="$HOME/$DIR/eth-langs.txt"
 rm "$OUT"
 
 LANGS=("Go" "C" "JavaScript" "Assembly" "Shell" "Java" "Sage" "M4" "NSIS" "Python" "Solidity" "Makefile" "HTML" "Dockerfile")
-LANGS_POSTFIX=("go" "c$|h" "js" "s" "sh" "java" "sage" "m4" "nsis" "py" "sol" "Makefile" "html" "Dockerfile")
+LANGS_POSTFIX=("\.go$" "\.[ch]$" "\.js$" "\.s$" "\.sh$" "\.java$" "\.sage$" "\.m4$" "\.ns[ih]$" "\.py$" "\.sol$" "Makefile$" "html$" "Dockerfile[.alltools]*$")
 
 for ((i = 0; i < ${#LANGS_POSTFIX[@]}; i++)); do
     LANG="${LANGS[i]}"
     POSTFIX="${LANGS_POSTFIX[i]}"
     cd "$ETH" || exit 1
-    git ls-files | grep "\.$POSTFIX$" | xargs wc -l | grep "total$" | awk -v lang="$LANG" '{print lang ": " $1}' >> "$OUT"
+    git ls-files | grep "$POSTFIX" | xargs wc -l | tail -n 1 | awk -v lang="$LANG" '{print lang ": " $1}' >> "$OUT"
 done
 
+# todo: file count with wc -l and awk '{print $1 - 1}', exept if 0 then 1
 # todo: total loc
-
+# todo: andel
 
 
 
