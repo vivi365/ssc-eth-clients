0747234331527b1b7a5a4faa281f9f03bc63b3f0
anderssonvivi <viviandersson0@gmail.com>
2024-04-01 12:34:45 +0200

    adds files count

--- scripts/source_stats.sh
+++ scripts/source_stats.sh
@@ -5,20 +5,29 @@ ETH="$HOME/go-ethereum"
 OUT="$HOME/$DIR/eth-langs.txt"
 rm "$OUT"
 
+# get langs by curl https://api.github.com/repos/ethereum/go-ethereum/languages
+
 LANGS=("Go" "C" "JavaScript" "Assembly" "Shell" "Java" "Sage" "M4" "NSIS" "Python" "Solidity" "Makefile" "HTML" "Dockerfile")
 LANGS_POSTFIX=("\.go$" "\.[ch]$" "\.js$" "\.s$" "\.sh$" "\.java$" "\.sage$" "\.m4$" "\.ns[ih]$" "\.py$" "\.sol$" "Makefile$" "html$" "Dockerfile[.alltools]*$")
 
 for ((i = 0; i < ${#LANGS_POSTFIX[@]}; i++)); do
+    cd "$ETH" || exit 1
     LANG="${LANGS[i]}"
     POSTFIX="${LANGS_POSTFIX[i]}"
-    cd "$ETH" || exit 1
-    git ls-files | grep "$POSTFIX" | xargs wc -l | tail -n 1 | awk -v lang="$LANG" '{print lang ": " $1 " (LOC)"}' >> "$OUT"
+    # get num files and loc
+    LOC=$(git ls-files | grep "$POSTFIX" | xargs wc -l | tail -n 1 | awk '{print $1}')
+    NUM_FILES=$(git ls-files | grep -c "$POSTFIX")
+    # write
+    echo "$LANG. FILES: $NUM_FILES LOC: $LOC" >> "$OUT"
 done
 
-TOTAL=$(awk '{sum += $2} END {print "Total:", sum}' "$OUT")
-echo "$TOTAL" >> "$OUT"
-# todo: file count with wc -l and awk '{print $1 - 1}', exept if 0 then 1
-# todo: total loc
+# write total files and loc
+TOTAL_FILES=$(awk '{sum += $3} END {print "Total FILES:", sum}' "$OUT")
+TOTAL_LOC=$(awk '{sum += $5} END {print "Total LOC:", sum}' "$OUT")
+echo -e "\n$TOTAL_FILES" >> "$OUT"
+echo "$TOTAL_LOC" >> "$OUT"
+
+
 # todo: andel
 
 
