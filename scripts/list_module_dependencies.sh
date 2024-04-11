5f9f3ceb64625cea52567e7b4bbf2931307151de
anderssonvivi <viviandersson0@gmail.com>
2024-04-11 13:12:46 +0200

    too simple

--- scripts/list_module_dependencies.sh
+++ scripts/list_module_dependencies.sh
@@ -6,11 +6,11 @@ if [ $# -ne 1 ]; then
 fi
 
 SRC_DIR=~/$1
-DIR=~/ssc-eth-clients/data/dependencies
+DIR=~/ssc-eth-clients/data/dependencies/reports
 FILE_NAME="all_deps_$1_$(date +%Y-%m-%d-%H).txt"
 
 mkdir -p "$DIR"
 cd "$SRC_DIR" || exit 1
 
 go list -m all | awk 'NR > 1 { print }' > "$DIR/$FILE_NAME"
-echo "Total dependencies: $(wc -l < "$TARGET_FILE")"
+echo "Total dependencies: $(wc -l < "$DIR/$FILE_NAME")"
