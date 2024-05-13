da1b76379e4a089aae0c1426c61d38b85c09cb7b
vivi365 <viviandersson0@gmail.com>
2024-05-13 14:30:09 +0200

    .

--- scripts/README.md
+++
@@ -1,3 +0,0 @@
-This document specifies what each scripts does.
-
-how to reproduce: specify commit (v.1.13.4 hash )
--- scripts/repo-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DIR="ssc-eth-clients/data/repo"
-curl -o "$HOME/$DIR/eth-langs-raw.json" https://api.github.com/repos/ethereum/go-ethereum/languages
-
-# todo api via js
